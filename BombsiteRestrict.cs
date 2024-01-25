e8d9bb3b7fbdd9fe0f4e72ffbe5fcaef01fe26eb
NockyCZ <63038995+NockyCZ@users.noreply.github.com>
2024-01-25 19:02:15 +0100

    Update v1.0.6

--- BombsiteRestrict.cs
+++ BombsiteRestrict.cs
@@ -4,7 +4,7 @@ using CounterStrikeSharp.API.Core.Attributes;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
 using CounterStrikeSharp.API.Modules.Utils;
 using System.Text.Json.Serialization;
-using CounterStrikeSharp.API.Modules.Timers;
+using CounterStrikeSharp.API.Modules.Commands;
 
 namespace BombsiteRestrict;
 
@@ -15,14 +15,17 @@ public class GenerateConfig : BasePluginConfig
     [JsonPropertyName("Count bots as players")] public bool bCountBots { get; set; } = false;
     [JsonPropertyName("Disabled site")] public int iDisabledSite { get; set; } = 0;
     [JsonPropertyName("Which team count as players")] public int iTeam { get; set; } = 0;
+    [JsonPropertyName("Send plant restrict message to team")] public int iMessageTeam { get; set; } = 0;
+    [JsonPropertyName("Allow center message")] public bool bAllowedCenter { get; set; } = true;
 }
 
 public class BombsiteRestrict : BasePlugin, IPluginConfig<GenerateConfig>
 {
     public override string ModuleName => "Bombsites Restrict";
     public override string ModuleAuthor => "Nocky";
-    public override string ModuleVersion => "1.0.5";
+    public override string ModuleVersion => "1.0.6";
     private static int g_iDisabledSite;
+    private static int g_iMessageTeam;
     private static bool g_bPluginDisabled;
 
     public GenerateConfig Config { get; set; } = null!;
@@ -32,16 +35,45 @@ public class BombsiteRestrict : BasePlugin, IPluginConfig<GenerateConfig>
     }
     public override void Load(bool hotReload)
     {
+        RegisterListener<Listeners.OnTick>(() =>
+        {
+            if (Config.bAllowedCenter && g_iDisabledSite != 0)
+            {
+                foreach (var p in Utilities.GetPlayers().Where(p => p.IsValid && !p.IsBot && !p.IsHLTV))
+                {
+                    string site = g_iDisabledSite == 1 ? "B" : "A";
+                    if (g_iMessageTeam == (byte)CsTeam.CounterTerrorist || g_iMessageTeam == (byte)CsTeam.Terrorist)
+                    {
+                        if (p.TeamNum == g_iMessageTeam)
+                        {
+                            p.PrintToCenterHtml($"{Localizer["Bombsite_Disabled_Center", site]}");
+                        }
+                    }
+                    else
+                    {
+                        p.PrintToCenterHtml($"{Localizer["Bombsite_Disabled_Center", site]}");
+                    }
+                }
+            }
+        });
         RegisterListener<Listeners.OnMapStart>(mapName =>
         {
             Server.NextFrame(() =>
             {
+                g_iDisabledSite = 0;
+                g_iMessageTeam = Config.iMessageTeam;
+                if (g_iMessageTeam == 1)
+                    g_iMessageTeam = (byte)CsTeam.CounterTerrorist;
+                else if (g_iMessageTeam == 2)
+                    g_iMessageTeam = (byte)CsTeam.Terrorist;
+
+
                 var Bombsites = Utilities.FindAllEntitiesByDesignerName<CBaseEntity>("func_bomb_target");
                 if (Bombsites.Count() != 2)
                 {
                     g_bPluginDisabled = true;
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("[Bombsites] The Bombsite Restrict is disabled, because there are no bomb plants on this map.");
+                    Console.WriteLine("[Bombsite Restrict] The Bombsite Restrict plugin is disabled, because there are no bomb plants on this map.");
                     Console.ResetColor();
                 }
                 else
@@ -51,42 +83,47 @@ public class BombsiteRestrict : BasePlugin, IPluginConfig<GenerateConfig>
             });
         });
     }
+
+    [ConsoleCommand("css_restrictbombsite", "Restrict bombsite for current map")]
+    [CommandHelper(1, "<site>", whoCanExecute: CommandUsage.SERVER_ONLY)]
+    public void RestrictBombsite_CMD(CCSPlayerController player, CommandInfo info)
+    {
+        var arg = info.GetArg(1);
+        if (!int.TryParse(arg, out int site))
+        {
+            info.ReplyToCommand($"[Bombsite Restrict] The site must be a number!");
+            return;
+        }
+        if (site < 1 && site > 2)
+        {
+            info.ReplyToCommand($"[Bombsite Restrict] The site must be a 1 (A) or 2 (B)!");
+            return;
+        }
+        string allowedSite = site == 1 ? "A" : "B";
+        info.ReplyToCommand($"[Bombsite Restrict] {allowedSite} plant was blocked on this map. If there are less than {Config.iMinPlayers} players.");
+        Config.iDisabledSite = site;
+    }
+
     [GameEventHandler(HookMode.Pre)]
-    public HookResult EventBombBeginplant(EventBombBeginplant @event, GameEventInfo info)
+    public HookResult OnRoundEnd(EventRoundEnd @event, GameEventInfo info)
     {
         if (g_bPluginDisabled)
             return HookResult.Continue;
 
-        if (g_iDisabledSite != 0)
+        var Sites = Utilities.FindAllEntitiesByDesignerName<CBaseEntity>("func_bomb_target");
+        foreach (var entity in Sites)
         {
-            var site = new CBombTarget(NativeAPI.GetEntityFromIndex(@event.Site));
-            int iSiteType = 1;
-            if (site.IsBombSiteB)
-                iSiteType = 2;
-
-            if (g_iDisabledSite == iSiteType)
-            {
-                CCSPlayerController player = @event.Userid;
-                Server.NextFrame(() =>
-                {
-                    player.PrintToChat($"{Localizer["Prefix"]} {Localizer["Warning_Message"]}");
-                    player.PrintToCenter($"{Localizer["Warning_Message_Center"]}");
-                });
-
-                var entity = new CEntityInstance(NativeAPI.GetEntityFromIndex(@event.Site));
-                entity.AcceptInput("Disable");
-
-                AddTimer(0.5f, () => { entity.AcceptInput("Enable"); }, TimerFlags.STOP_ON_MAPCHANGE);
-            }
+            entity.AcceptInput("Enable");
         }
+
         return HookResult.Continue;
     }
-
     [GameEventHandler(HookMode.Pre)]
     public HookResult OnRoundStart(EventRoundStart @event, GameEventInfo info)
     {
         if (g_bPluginDisabled)
             return HookResult.Continue;
+
         if (!GameRules().WarmupPeriod)
         {
             if (GetPlayersCount() <= Config.iMinPlayers)
@@ -99,7 +136,19 @@ public class BombsiteRestrict : BasePlugin, IPluginConfig<GenerateConfig>
                 }
                 g_iDisabledSite = iSite;
                 string site = g_iDisabledSite == 1 ? "B" : "A";
-                Server.PrintToChatAll($"{Localizer["Prefix"]} {Localizer["Bombsite_Disabled", site]}");
+                DisableBombsite();
+
+                if (g_iMessageTeam == (byte)CsTeam.CounterTerrorist || g_iMessageTeam == (byte)CsTeam.Terrorist)
+                {
+                    foreach (var player in Utilities.GetPlayers().Where(p => p.IsValid && !p.IsBot && !p.IsHLTV && p.TeamNum == g_iMessageTeam))
+                    {
+                        player.PrintToChat($"{Localizer["Prefix"]} {Localizer["Bombsite_Disabled", site]}");
+                    }
+                }
+                else
+                {
+                    Server.PrintToChatAll($"{Localizer["Prefix"]} {Localizer["Bombsite_Disabled", site]}");
+                }
             }
             else
             {
@@ -108,42 +157,45 @@ public class BombsiteRestrict : BasePlugin, IPluginConfig<GenerateConfig>
         }
         return HookResult.Continue;
     }
+    private static void DisableBombsite()
+    {
+        var Sites = Utilities.FindAllEntitiesByDesignerName<CBaseEntity>("func_bomb_target");
+        foreach (var entity in Sites)
+        {
+            var site = new CBombTarget(NativeAPI.GetEntityFromIndex((int)entity.Index));
+            int entitySite = site.IsBombSiteB ? 2 : 1;
+            if (entitySite == g_iDisabledSite)
+                entity.AcceptInput("Disable");
+        }
+    }
     private int GetPlayersCount()
     {
         int iCount = 0;
-        Utilities.GetPlayers().ForEach(player =>
+        foreach (var player in Utilities.GetPlayers().Where(p => p.IsValid && !p.IsHLTV && p.Connected == PlayerConnectedState.PlayerConnected))
         {
-            if (Config.iTeam == 1 && (CsTeam)player.TeamNum == CsTeam.CounterTerrorist)
+            if (Config.iTeam == 0 && (player.TeamNum == (byte)CsTeam.Terrorist || player.TeamNum == (byte)CsTeam.CounterTerrorist))
             {
-                if (!Config.bCountBots)
+                if (!Config.bCountBots && !player.IsBot)
                 {
-                    if (!player.IsBot)
-                        iCount++;
-                }
-                else
                     iCount++;
+                }
+                else if (Config.bCountBots)
+                {
+                    iCount++;
+                }
             }
-            else if (Config.iTeam == 2 && (CsTeam)player.TeamNum == CsTeam.Terrorist)
+            else if ((Config.iTeam == 1 || Config.iTeam == 2) && player.TeamNum == Config.iTeam + 1)
             {
-                if (!Config.bCountBots)
+                if (!Config.bCountBots && !player.IsBot)
                 {
-                    if (!player.IsBot)
-                        iCount++;
-                }
-                else
                     iCount++;
-            }
-            else if (Config.iTeam == 0 && (CsTeam)player.TeamNum == CsTeam.Terrorist || (CsTeam)player.TeamNum == CsTeam.CounterTerrorist)
-            {
-                if (!Config.bCountBots)
+                }
+                else if (Config.bCountBots)
                 {
-                    if (!player.IsBot)
-                        iCount++;
-                }
-                else
                     iCount++;
+                }
             }
-        });
+        }
         return iCount;
     }
     internal static CCSGameRules GameRules()
--- README.md
+++ README.md
@@ -6,24 +6,12 @@ This plugin restricts the bombsites of each round if there are less than X playe
 Configuration in
 ```configs/plugins/BombsiteRestrict/BombsiteRestrict.json```
 
-<<<<<<< Updated upstream
 |   | What it does |
 | ------------- | ------------- |
 | `Minimum players`  | Minimum number of players to disable random planting |
-| `Disable plant message`  | Message sent at the beginning of the round if planting is disabled |
-| `Warning message chat/center` | Message if a player attempts to plant the bomb on a disabled site |
 | `Count bots as players` | If bots will be counted as valid players (`true` or `false`) |
-| `Message type` | Where will the warning message be sent (`0` - Chat , `1` - Center , `2` - Both) |
 | `Disabled site` | Which bombsite will be disabled (`0` - Random , `1` - A , `2` - B) |
 | `Which team count as players` | It allows setting which team will be considered as the 'Minimum players'. (`0` - Both teams , `1` - Only CT , `2` - Only T)|
-=======
-|                               | What it does                                                                                                                |
-| ----------------------------- | --------------------------------------------------------------------------------------------------------------------------- |
-| `Minimum players`             | Minimum number of players to disable random planting                                                                        |
-| `Count bots as players`       | If bots will be counted as valid players (`true` or `false`)                                                                |
-| `Disabled site`               | Which bombsite will be disabled (`0` - Random , `1` - A , `2` - B)                                                          |
-| `Which team count as players` | It allows setting which team will be considered as the 'Minimum players'. (`0` - Both teams , `1` - Only CT , `2` - Only T) |
->>>>>>> Stashed changes
 
 ### Installation
 1. Download the lastest release https://github.com/NockyCZ/CS2_BombsitesRestrict/releases
